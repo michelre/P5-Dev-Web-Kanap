2b002ea9585cd4c3294c722a5b3288de3353d59a
Mar-Aze <97227237+Mar-Aze@users.noreply.github.com>
2022-03-25 11:20:35 +0100

    Indentation, add Product ID ds <a>

--- front/js/script.js
+++ front/js/script.js
@@ -4,13 +4,13 @@ fetch("http://localhost:3000/api/products")
     .then( jsonListProducts => {
         for(let jsonProducts of jsonListProducts){
             let product = new Product(jsonProducts);
-            document.querySelector(".items").innerHTML += `<a href="./product.html?id=42">
-                                                                <article>
-                                                                    <img src="${product.imageUrl}" alt="${product.altTxt}">
-                                                                    <h3 class="productName">${product.name}</h3>
-                                                                    <p class="productDescription">${product.description}</p>
-                                                                </article>
-                                                            </a>`
+            document.querySelector(".items").innerHTML += 
+                `<a href="./product.html?id=${product.id}">
+                    <article>
+                        <img src="${product.imageUrl}" alt="${product.altTxt}">
+                        <h3 class="productName">${product.name}</h3>
+                        <p class="productDescription">${product.description}</p>
+                    </article>
+                </a>`
         }
-
     })
